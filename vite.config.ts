6e4bdd4407b2db7321cd2c0c35a17c7b3b3d0249
lelnotwellyet <161947236+lelnotwellyet@users.noreply.github.com>
2024-10-02 21:47:59 +0530

    tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      'string-width': 'string-width/index.js',
+    },
+  },
+  optimizeDeps: {
+    include: ['string-width', 'wide-align'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true, // Ensure mixed ESM/CommonJS modules are handled
+    },
