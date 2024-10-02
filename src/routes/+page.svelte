a0f52dbc39eae3d545c3e44095f8f3c91da9cc41
lelnotwellyet <161947236+lelnotwellyet@users.noreply.github.com>
2024-10-02 21:33:17 +0530

    tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to svelte</h1>
-<p>
+<p class="font-red marker:first-letter:">
   <slot></slot>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read
   the documentation
 </p>
